913c77bbee9cac81bc5490ae35abae9c098a733a
Eric <605733730@qq.com>
2015-10-22 18:30:44 +0800

    fixed super not called exception

--- src/com/xlee/activity/MainActivity.java
+++ src/com/xlee/activity/MainActivity.java
@@ -139,6 +139,7 @@ public class MainActivity extends Activity {
         if (null != mJumpingBeans) {
             mJumpingBeans.stopJumping();
         }
+        super.onPause();
     }
 
     @Override
